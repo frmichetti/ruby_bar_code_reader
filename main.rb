8942bb46ca438f5946e5e66a2ac24758b68413f2
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-02 14:18:51 -0300

    serve static files

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'rghost'
 require 'zipruby'
 require 'zxing'
-require "mini_magick"
+require 'mini_magick'
+require 'securerandom'
+
 
 
 
@@ -12,22 +14,35 @@ class Image
       @path = path
     end
   end
+  
 
 get '/' do 
-  'Hello World!'
+  send_file File.join(settings.public_folder, 'index.html')
 end  
 
-get '/crop' do
+post '/crop' do
+       
+  attach_path = nil  
+
+  unless params[:file]
+    content_type :json
+    return [400, {msg: "File is not provided"}.to_json]    
+  end  
+    
+  attach_path = params[:file][:tempfile].path
   
-image = MiniMagick::Image.open("data/boleto20220510-51214-e07e6a.png")
-image.path #=> "/var/folders/k7/6zx6dx6x7ys3rv3srh0nyfj00000gn/T/magick20140921-75881-1yho3zc.jpg"
+  uuid = SecureRandom.uuid
+  
+image = MiniMagick::Image.open(attach_path)
+
 image.format "png"
-image.resize "1920x1080"
+
 image.rotate "-180"
 image.crop "100%x25%+0+0"
 image.rotate "-180"
-
-image.write "data/output.png"
+image.resize "1280x720"
+image.write "output/#{uuid}.png"
+send_file(image.path, :filename => "#{params[:file][:filename].split('.')[0]}.png", :type => "image/png")
 end
 
 post '/decode' do     
